481d30eeb373349fc2344f4c02398b128befe10b
Bola Ghaly <polaghaly63@gmail.com>
2022-04-12 20:27:04 -0400

    Add 13-isPalindrome

---
+++ 13-isPalindrome/isPalindrome.ts
@@ -0,0 +1,12 @@
+export function isPalindrome(word: any) {
+  const len = word.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      word = false;
+      return word;
+    } else if (word[i] === word[len - 1 - i]) {
+      word = true;
+    }
+  }
+  return word;
+}
